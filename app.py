aa9a43532a45aaf1195752445722e92b13a1be85
Prashant Shukla <97425883+Prashant2091@users.noreply.github.com>
2022-11-27 21:45:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ else:
 	test_image = Image.open(file)
 
 	st.image(test_image, caption="Input Image", width = 400)
-	pred = predict_class(np.asarray(model.predict(test_image), model))
+	pred = np.asarray(model.predict(test_image), model)
 
 	class_names = ['butterfly', 'cow', 'elephant', 'sheep', 'squirrel']
 
