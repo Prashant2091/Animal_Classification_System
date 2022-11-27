7127d1a93e2295387cdb2cf1a1af926084d97065
Prashant Shukla <97425883+Prashant2091@users.noreply.github.com>
2022-11-27 19:37:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.set_option('deprecation.showfileUploaderEncoding', False)
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-	model = tf.keras.models.load_model('./animal_model_trained.hdf5')
+	model = tf.keras.models.load_model('animal_model_trained.hdf5')
 	return model
 
 
