0a4d5158e9311843d72b44a93bb7ff9625db5509
Prashant Shukla <97425883+Prashant2091@users.noreply.github.com>
2022-11-27 19:11:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,32 +7,56 @@ Created on Sun Nov 27 00:44:37 2022
 
 import streamlit as st
 import tensorflow as tf
+import numpy as np
+from PIL import Image
+
+
+st.set_option('deprecation.showfileUploaderEncoding', False)
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model=tf.keras.models.load_model(r'plant_classifier.hdf5')
-  return model
-model=load_model()
-st.write("""
-# Plant Leaf Detection System"""
-)
-file=st.file_uploader("Choose plant photo from computer",type=["jpg","png"])
-import cv2
-from PIL import Image,ImageOps
-import numpy as np
-def import_and_predict(image_data,model):
-    size=(64,64)
-    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
-    img=np.asarray(image)
-    img_reshape=img[np.newaxis,...]
-    prediction=model.predict(img_reshape)
-    return prediction
+	model = tf.keras.models.load_model('./animal_model_trained.hdf5')
+	return model
+
+
+def predict_class(image, model):
+
+	image = tf.cast(image, tf.float32)
+	image = tf.image.resize(image, [188, 188])
+
+	image = np.expand_dims(image, axis = 0)
+
+	prediction = model.predict(image)
+
+	return prediction
+
+
+model = load_model()
+st.title('Animal Classifier')
+
+file = st.file_uploader("Upload an image of an animal", type=["jpg", "png"])
+
+
 if file is None:
-    st.text("Please upload an image file")
+	st.text('Waiting for upload....')
+
 else:
-    image=Image.open(file)
-    st.image(image,use_column_width=True)
-    prediction=import_and_predict(image,model)
-    class_names=['Alpinia Galanga (Rasna)','Amaranthus Viridis (Arive-Dantu)','Artocarpus Heterophyllus (Jackfruit)','Azadirachta Indica (Neem)','Basella Alba (Basale)','Brassica Juncea (Indian Mustard)','Carissa Carandas (Karanda)','Citrus Limon (Lemon)','Ficus Auriculata (Roxburgh fig)','Ficus Religiosa (Peepal Tree)','Hibiscus Rosa-sinensis','Jasminum (Jasmine)','Mangifera Indica (Mango)','Mentha (Mint)','Moringa Oleifera (Drumstick)','Muntingia Calabura (Jamaica Cherry-Gasagase)','Murraya Koenigii (Curry)','Nerium Oleander (Oleander)','Nyctanthes Arbor-tristis (Parijata)','Ocimum Tenuiflorum (Tulsi)','Piper Betle (Betel)','Plectranthus Amboinicus (Mexican Mint)','Pongamia Pinnata (Indian Beech)','Psidium Guajava (Guava)','Punica Granatum (Pomegranate)','Santalum Album (Sandalwood)','Syzygium Cumini (Jamun)','Syzygium Jambos (Rose Apple)','Tabernaemontana Divaricata (Crape Jasmine)','Trigonella Foenum-graecum (Fenugreek)']
-    string="OUTPUT : "+class_names[np.argmax(prediction)]
-    st.success(string)
+	slot = st.empty()
+	slot.text('Running inference....')
+
+	test_image = Image.open(file)
+
+	st.image(test_image, caption="Input Image", width = 400)
+
+	pred = predict_class(np.asarray(test_image), model)
+
+	class_names = ['butterfly', 'cow', 'elephant', 'sheep', 'squirrel']
+
+	result = class_names[np.argmax(pred)]
+
+	output = 'The image is a ' + result
+
+	slot.text('Done')
+
+	st.success(output)
+
