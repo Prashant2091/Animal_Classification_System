cbd30cee99b4d8becb474a3a77a7763b8625bb1d
Prashant Shukla <97425883+Prashant2091@users.noreply.github.com>
2022-11-30 15:15:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ Created on Sun Nov 27 00:44:37 2022
 
 @author: Prashant
 """
-import os
+'''import os
 import streamlit as st
 import tensorflow as tf
 import numpy as np
@@ -61,5 +61,57 @@ else:
 
 	slot.text('Done')
 
-	st.success(output)
+	st.success(output)'''
 
+
+import streamlit as st
+from PIL import Image
+import matplotlib.pyplot as plt
+import tensorflow_hub as hub
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras.models import *
+from tensorflow.keras import preprocessing
+import time
+fig = plt.figure()
+st.title('Green stem Classifier')
+st.markdown("Prediction :{butterfly,cow,elephant,sheep,squirrel)")
+def main():
+    file_uploaded = st.file_uploader("Choose File", type=["png","jpg","jpeg"])
+    class_btn = st.button("Classify")
+    if file_uploaded is not None:    
+        image = Image.open(file_uploaded)
+        st.image(image, caption='Uploaded Image', use_column_width=True)
+        
+    if class_btn:
+        if file_uploaded is None:
+            st.write("Invalid command, please upload an image")
+        else:
+            with st.spinner('Model working....'):
+                 plt.imshow(image)
+                 plt.axis("off")
+                 predictions = predict(image)
+                 time.sleep(1)
+                 st.success('Classified')
+                 st.write(predictions)
+		
+		
+		
+
+def predict(image):
+    classifier_model = "animal_model_trained.hdf5"
+      
+    model = load_model(classifier_model)
+      
+    test_image = image.resize((188,188))
+    test_image = preprocessing.image.img_to_array(test_image)
+    test_image = test_image / 255.0
+    test_image = np.expand_dims(test_image, axis=0)
+    class_names = ['butterfly', 'cow', 'elephant', 'sheep', 'squirrel']
+    predictions = model.predict(test_image)
+    scores = tf.nn.softmax(predictions[0])
+    scores = scores.numpy()
+    result = f"{class_names[np.argmax(scores)]} with a { (100 *       np.max(scores)).round(2) } % confidence." 
+
+    return result
