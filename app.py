d520e8cc2e768cb3363e06f5810408e3cb80fcf8
Prashant Shukla <97425883+Prashant2091@users.noreply.github.com>
2022-11-30 15:47:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -121,4 +121,4 @@ def predict(image):
     result = f"{class_names[np.argmax(scores)]} with a { (100 * np.max(scores)).round(2) } % confidence." 
 
     return result
-predict(image)
+#predict(image)
