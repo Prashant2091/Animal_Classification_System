fb54305c77b0f4894891d9da188fd6d9d7505d37
Prashant Shukla <97425883+Prashant2091@users.noreply.github.com>
2022-11-30 15:20:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ else:
 import streamlit as st
 from PIL import Image
 import matplotlib.pyplot as plt
-import tensorflow_hub as hub
+#import tensorflow_hub as hub
 import tensorflow as tf
 import numpy as np
 from tensorflow import keras
