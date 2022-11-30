7fe61a1f97e5d6398df167722bc581baca26b92c
Prashant Shukla <97425883+Prashant2091@users.noreply.github.com>
2022-11-30 15:24:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -77,14 +77,14 @@ import time
 fig = plt.figure()
 st.title('Animal Classifier amongst butterfly,cow,elephant,sheep,squirrel')
 st.markdown("Prediction :{butterfly,cow,elephant,sheep,squirrel)")
-def main():
-    file_uploaded = st.file_uploader("Choose File", type=["png","jpg","jpeg"])
-    class_btn = st.button("Classify")
-    if file_uploaded is not None:    
+#def main():
+file_uploaded = st.file_uploader("Choose File", type=["png","jpg","jpeg"])
+class_btn = st.button("Classify")
+if file_uploaded is not None:    
         image = Image.open(file_uploaded)
         st.image(image, caption='Uploaded Image', use_column_width=True)
         
-    if class_btn:
+if class_btn:
         if file_uploaded is None:
             st.write("Invalid command, please upload an image")
         else:
