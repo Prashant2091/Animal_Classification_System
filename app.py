7979afa68973a65503ceb95ffeec4ee805378c85
Prashant Shukla <97425883+Prashant2091@users.noreply.github.com>
2023-08-01 23:52:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,56 +16,38 @@ st.set_option('deprecation.showfileUploaderEncoding', False)
 @st.cache(allow_output_mutation=True)
 
 @st.experimental_singleton
-def load_model():
-    
-  if not os.path.isfile('animal_model_trained.hdf5'):
+def load_model():  
+ if not os.path.isfile('animal_model_trained.hdf5'):
     subprocess.run(['curl --output model.hdf5 "https://github.com/Prashant2091/Animal_Classification_System/blob/main/animal_model_trained.hdf5"'], shell=True)
     return tf.keras.models.load_model('model.hdf5', compile=False)
-
 def predict_class(image, model):
-
-	image = tf.cast(image, tf.float32)
-	image = tf.image.resize(image, [188, 188])
-
-	#image = np.expand_dims(image, axis = 0)
-	image = image[np.newaxis,...]
-
-	prediction = model.predict(image)
-
-	return prediction
-
-
+ image = tf.cast(image, tf.float32)
+ image = tf.image.resize(image, [188, 188])
+ #image = np.expand_dims(image, axis = 0)
+ image = image[np.newaxis,...]
+ prediction = model.predict(image)
+ return prediction
 model = load_model()
 st.title('Animal Classifier')
-
 file = st.file_uploader("Upload an image of an animal", type=["jpg", "png"])
-
-
 if file is None:
-	st.text('Waiting for upload....')
-
+ st.text('Waiting for upload....')
 else:
-	slot = st.empty()
-	slot.text('Running inference....')
-                                             
-	test_image = Image.open(file)
-
-	st.image(test_image, caption="Input Image", width = 400)
-	#pred = predict_class(np.asarray(test_image), model)
-	#pred=model.predict(test_image).argmax()
-	if model is not None:
-         pred = model.predict(test_image).argmax()
-        else:
-         st.write("Model not loaded properly. Check the model loading process.")
-	class_names = ['butterfly', 'cow', 'elephant', 'sheep', 'squirrel']
-
-	result = class_names[np.argmax(pred)]
-
-	output = 'The image is a ' + result
-
-	slot.text('Done')
-
-	st.success(output)
+ slot = st.empty()
+ slot.text('Running inference....')
+ test_image = Image.open(file)
+ st.image(test_image, caption="Input Image", width = 400)
+ #pred = predict_class(np.asarray(test_image), model)
+ #pred=model.predict(test_image).argmax()
+if model is not None:
+ pred = model.predict(test_image).argmax()
+else:
+ st.write("Model not loaded properly. Check the model loading process.")
+class_names = ['butterfly', 'cow', 'elephant', 'sheep', 'squirrel']
+result = class_names[np.argmax(pred)]
+output = 'The image is a ' + result
+slot.text('Done')	
+st.success(output)
 
 '''import os
 import streamlit as st
